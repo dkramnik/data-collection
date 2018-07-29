93157e63a580ba5e6983cdfc5f69690f4b14833b
dkramnik <kramnik@mit.edu>
2018-07-29 02:22:10 -0400

    Changed file location

--- demo-scripts/thesis-ch3/sweep_process_data.m
+++ demo-scripts/thesis-ch3/sweep_process_data.m
@@ -3,7 +3,7 @@ clear
 clc
 
 %file_name = 'test-AQC-sweep-data_07-28-2018_17-40-48.mat';
-file_name = 'cryo-run1-AQC-sweep-data_294K.mat';
+file_name = 'cryo-run1-AQC-sweep-data_160K.mat';
 
 load( file_name );   
 
